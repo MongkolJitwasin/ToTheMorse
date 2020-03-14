4b70bde71b6f68c94d9c4a4361a7063d47c7a8ab
Mongkol Jitwasin <ambush10side@gmail.com>
2020-03-14 21:50:01 +0700

    Add MorseConverter and MorseSymbols.
    Add MorseConverter class and MorseSymbols enum.

---
+++ ToTheMorseLib/ToTheMorseLib/MorseConverter.swift
@@ -0,0 +1,16 @@
+//
+//  MorseConverter.swift
+//  ToTheMorseLib
+//
+//  Created by Mongkol Jitwasin on 14/3/2563 BE.
+//  Copyright © 2563 Mongkol Jitwasin. All rights reserved.
+//
+
+import Foundation
+
+internal class MorseConverter {
+    
+    
+    
+    
+}
---
+++ ToTheMorseLib/ToTheMorseLib/MorseSymbols.swift
@@ -0,0 +1,18 @@
+//
+//  MorseSymbols.swift
+//  ToTheMorseLib
+//
+//  Created by Mongkol Jitwasin on 14/3/2563 BE.
+//  Copyright © 2563 Mongkol Jitwasin. All rights reserved.
+//
+
+import Foundation
+
+internal enum MorseSymbols {
+    case dit
+    case dah
+    
+    case internalSpace
+    case letterSpace
+    case wordSpace
+}

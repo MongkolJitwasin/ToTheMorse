1e58076716fe638203edbd2ab83837d0668930ea
Mongkol Jitwasin <ambush10side@gmail.com>
2020-03-19 13:11:53 +0700

    add MorseLanguage class and update MorseConverter

--- ToTheMorseLib/ToTheMorseLib/MorseLanguage.swift
+++ ToTheMorseLib/ToTheMorseLib/MorseLanguage.swift
@@ -1,8 +1,8 @@
 //
-//  MorseSymbols.swift
+//  MorseLanguage.swift
 //  ToTheMorseLib
 //
-//  Created by Mongkol Jitwasin on 19/3/2563 BE.
+//  Created by Mongkol Jitwasin on 18/3/2563 BE.
 //  Copyright © 2563 Mongkol Jitwasin. All rights reserved.
 //
 

74d7773a8b8a1c80c5b546943ecd80830131702e
Mongkol Jitwasin <ambush10side@gmail.com>
2020-03-19 20:58:03 +0700

    Simplify code in MorseConverter class

--- ToTheMorseLib/ToTheMorseLib/MorseConverter.swift
+++ ToTheMorseLib/ToTheMorseLib/MorseConverter.swift
@@ -8,49 +8,61 @@
 
 import Foundation
 
-internal class MorseConverter {
+public class MorseConverter {
     
     private init() {}
     
-    func convertTextToMorseAsString(text: String, newLineEverWord: Bool) -> String {
-        var product = String()
+    public static func convertTextToMorseCode(text: String, newLineEveryWord: Bool) -> String {
+        var morseAsString = String()
         let morse = MorseLanguage()
         
+        let text = text.lowercased()
+        
         for eachChar in text {
-            let eachChar = eachChar.lowercased()
-            if (eachChar == " " && eachChar != text.first?.lowercased() && newLineEverWord) {
-                product.append("\r\n")
-            } else {
-                product.append(morse.morseCode[Character(eachChar)]!)
-                product.append("_")
+            
+            if let letter = morse.morseCode[eachChar] {
+                
+                if(letter == "/" && newLineEveryWord) {
+                    morseAsString.append("\r\n")
+                } else {
+                    morseAsString.append(letter + " ")
+                }
             }
+            
         }
         
-        return product
+        return morseAsString
     }
         
     
-    func convertMorseAsStringToText(morseCode: String) -> String {
+    public static func convertMorseCodeToText(morseCode: String) -> String {
         
         var product = String()
         var charAsMorseCode = String()
-        var separatorCount: Int = 0
+        var morseCode = morseCode
+        
+        if(morseCode.first == " ") {
+            morseCode.remove(at: morseCode.startIndex)
+        }
         
         for eachSymbol in morseCode {
-            if(eachSymbol != "_") {
+            let symbolIsMorseLanguage = (eachSymbol == "." || eachSymbol == "-" || eachSymbol == "/" || eachSymbol == "\r\n" || eachSymbol == " ")
+            
+            if(symbolIsMorseLanguage) {
                 
-                if(separatorCount == 1) {
-                    separatorCount = 0
-                    product.append(getChar(byCharAsMorseCode: charAsMorseCode))
+                if (eachSymbol == " ") {
+                    
+                    product.append(getLetter(byCharAsMorseCode: charAsMorseCode))
                     charAsMorseCode = String()
-                }
-                
-                charAsMorseCode.append(eachSymbol)
-            } else {
-                separatorCount = separatorCount + 1
-                
-                if(separatorCount == 3) {
+                    
+                } else if(eachSymbol == "\r\n") {
+                    
                     product.append(" ")
+                    
+                } else {
+                    
+                    charAsMorseCode.append(eachSymbol)
+                    
                 }
             }
         }
@@ -58,9 +70,10 @@ internal class MorseConverter {
         return product
     }
     
-    private func getChar(byCharAsMorseCode morseCode: String) -> Character {
+    private static func getLetter(byCharAsMorseCode morseCode: String) -> Character {
         let morseLanguage = MorseLanguage()
         let product = (morseLanguage.morseCode as NSDictionary).allKeys(for: morseCode) as! [Character]
+        
         return product[0]
     }
 }
